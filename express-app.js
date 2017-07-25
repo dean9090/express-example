2ed0024c014b5eb313a15c4e4e47ce73c26f054a
Gavin Stark <gavin@gstark.com>
2017-07-25 11:48:35 -0400

    Uses public folder for css (and js/img/etc)

--- express-app.js
+++ express-app.js
@@ -2,6 +2,11 @@ const express = require('express')
 
 const app = express()
 
+// Use the middleware (i.e plugin) for static files
+// and tell express they are in the 'public' folder
+app.use(express.static('public'))
+app.set('views', 'templates')
+
 // app.get is similar to our old friend addEventListener
 // except we are listening for requests from a
 // browser
@@ -10,12 +15,30 @@ const app = express()
 //
 // })
 
+app.get('/jason.json', (request, response) => {
+  const data = {
+    name: 'Jason',
+    when: new Date(),
+    what: 'loves fooberries'
+  }
+
+  response.json(data)
+})
+
 app.get('/jason', (request, response) => {
   response.send(`
-    Jason loves fooberries at ${new Date()}
-    <p>
-       <a href="/">Home</a>
-     </p>
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+        <p>Jason loves fooberries at ${new Date()}</p>
+        <p>
+           <a href="/">Home</a>
+         </p>
+      </body>
+    </html>
   `)
 })
 
